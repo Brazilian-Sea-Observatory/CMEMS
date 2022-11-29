3ec7c8516789a63578dd58b242618ad4b0f77a2c
guifranz <guifranz@gmail.com>
2022-11-29 15:26:19 -0300

    Update CMEMS2HDF5.sh

--- GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.sh
+++ GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# Carregar variáveis de ambiente
-
-. /etc/profile
-. ~/.bash_profile
-. ~/.bashrc
-
 # Vai para o diretório de execução do script
-cd /home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/
-
-# Testes
-echo " " >> /home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.log
-echo "-------" >> /home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.log
-echo $LDFLAGS >> /home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.log
+cd /home/guilherme.franz/Aplica/work/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY
 
 # Executa
-
-/home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.py >> /home/mohid/Aplica/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.log 2>&1
-
-# printenv > /tmp/printenv.log
+python3 /home/guilherme.franz/Aplica/work/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.py >> /home/guilherme.franz/Aplica/work/CMEMS/GLOBAL_ANALYSIS_FORECAST_PHY/CMEMS2HDF5.log 2>&1
+                                                                
